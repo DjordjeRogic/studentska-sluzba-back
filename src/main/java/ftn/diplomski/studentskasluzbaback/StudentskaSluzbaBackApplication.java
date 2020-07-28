71a61611f3a40abddbd81347115bde9cd24f5694
DjordjeRogic <rogic.djordje@gmail.com>
2020-07-28 14:07:24 +0200

    Initial commit

---
+++ src/main/java/ftn/diplomski/studentskasluzbaback/StudentskaSluzbaBackApplication.java
@@ -0,0 +1,13 @@
+package ftn.diplomski.studentskasluzbaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentskaSluzbaBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentskaSluzbaBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ftn/diplomski/studentskasluzbaback/StudentskaSluzbaBackApplicationTests.java
@@ -0,0 +1,13 @@
+package ftn.diplomski.studentskasluzbaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentskaSluzbaBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
